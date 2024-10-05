79d45cb9c2e7c0df8a3a25bf1c011b4beb4310ae
jai-singh5 <jaisingh09520@gmail.com>
2024-10-05 23:25:54 +0530

    adding newsitem

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,35 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Link } from "react-router-dom";
 
-export class Navbar extends Component {
-    render() {
-        return (
-            <div>
-                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <div class="container-fluid ">
-                        <a class="navbar-brand" href="/">
-                            <img src="logo.jpg" width={35} alt="" /> News Panda</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="/">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/about">About</a>
-                                </li>
-                            </ul>
-                        </div>
+const NavBar = () => {
+
+    return (
+        <div>
+            <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">NewsMonkey</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
+                        </ul>
                     </div>
-                </nav>
-            </div>
-        )
-    }
+                </div>
+            </nav>
+        </div>
+    )
+
 }
 
-export default Navbar
+export default NavBar
--- src/components/News.js
+++ src/components/News.js
@@ -1,13 +1,97 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { useEffect, useState } from 'react';
+import NewsItem from './NewsItem';
+import Spinner from './Spinner';
+import PropTypes from 'prop-types';
+import InfiniteScroll from "react-infinite-scroll-component";
 
-export default class News extends Component {
-  render() {
+const News = ({ apiKey, country = 'us', pageSize = 8, category = 'general', setProgress }) => {
+    const [articles, setArticles] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1);
+    const [totalResults, setTotalResults] = useState(0);
+    const capitalizeFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    const updateNews = async () => {
+        setProgress(10);
+        const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${page}&pageSize=${pageSize}`;
+        setLoading(true);
+        try {
+            let data = await fetch(url);
+            setProgress(30);
+            let parsedData = await data.json();
+            console.log("API Response:", parsedData);
+            setProgress(70);
+            setArticles(parsedData.articles);
+            setTotalResults(parsedData.totalResults);
+        } catch (error) {
+            console.error("Error fetching news:", error);
+        } finally {
+            setLoading(false);
+            setProgress(100);
+        }
+    }
+
+    useEffect(() => {
+        document.title = `${capitalizeFirstLetter(category)} - NewsMonkey`;
+        updateNews();
+        // eslint-disable-next-line
+    }, []);
+
+    const fetchMoreData = async () => {
+        const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${page + 1}&pageSize=${pageSize}`;
+        setPage(page + 1);
+        try {
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            setArticles(articles.concat(parsedData.articles));
+            setTotalResults(parsedData.totalResults);
+        } catch (error) {
+            console.error("Error fetching more news:", error);
+        }
+    };
+
     return (
-      <div>
-        This is a News Component
-        <NewsItem />
-      </div>
-    )
-  }
-}
+        <>
+            <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>NewsMonkey - Top {capitalizeFirstLetter(category)} Headlines</h1>
+            {loading && <Spinner />}
+            <InfiniteScroll
+                dataLength={articles.length}
+                next={fetchMoreData}
+                hasMore={articles.length !== totalResults}
+                loader={<Spinner />}
+            >
+                <div className="container">
+                    <div className="row">
+                        {articles.map((element) => {
+                            return (
+                                <div className="col-md-4" key={element.url}>
+                                    <NewsItem
+                                        title={element.title ? element.title : ""}
+                                        description={element.description ? element.description : ""}
+                                        imageUrl={element.urlToImage}
+                                        newsUrl={element.url}
+                                        author={element.author}
+                                        date={element.publishedAt}
+                                        source={element.source.name}
+                                    />
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+            </InfiniteScroll>
+        </>
+    );
+};
+
+News.propTypes = {
+    apiKey: PropTypes.string.isRequired,
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+    setProgress: PropTypes.func.isRequired,
+};
+
+export default News;
