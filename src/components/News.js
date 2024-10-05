0cf8e5b24edc581c2c87ce36f1e7b7e5d5fee407
jai-singh5 <jaisingh09520@gmail.com>
2024-10-05 21:44:20 +0530

    first commit

---
+++ src/components/News.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export default class News extends Component {
+  render() {
+    return (
+      <div>
+        This is a News Component
+        <NewsItem />
+      </div>
+    )
+  }
+}
